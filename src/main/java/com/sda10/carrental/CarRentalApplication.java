289cf0eb0b285bfa686f04dd0acace5ccc4fb86d
Calota Alexandru <calotaalexandrug@gmail.com>
2019-12-08 09:59:16 +0200

    Renamed package

--- src/main/java/com/sda10/carrental/CarRentalApplication.java
+++ src/main/java/com/sda10/carrental/CarRentalApplication.java
@@ -1,4 +1,4 @@
-package com.sda10.carrental.carrental;
+package com.sda10.carrental;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/sda10/carrental/CarRentalApplicationTests.java
+++ src/test/java/com/sda10/carrental/CarRentalApplicationTests.java
@@ -1,4 +1,4 @@
-package com.sda10.carrental.carrental;
+package com.sda10.carrental;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
