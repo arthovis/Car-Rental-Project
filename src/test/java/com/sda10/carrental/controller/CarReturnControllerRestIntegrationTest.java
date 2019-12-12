30b9095ae168c3a6e7f668c7662212f547e44ba9
DanNistor1 <dan.nistor1@yahoo.ro>
2019-12-12 18:14:28 +0200

    refactor CarReturnControllerRestIntegrationTest

--- src/test/java/com/sda10/carrental/controller/CarReturnControllerRestIntegrationTest.java
+++ src/test/java/com/sda10/carrental/controller/CarReturnControllerRestIntegrationTest.java
@@ -4,8 +4,7 @@ import com.sda10.carrental.RestIntegrationTest;
 import com.sda10.carrental.dto.CarReturnDto;
 import com.sda10.carrental.model.CarReturn;
 import com.sda10.carrental.repository.CarReturnRepository;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
@@ -22,98 +21,98 @@ public class CarReturnControllerRestIntegrationTest extends RestIntegrationTest 
     @Autowired
     private CarReturnRepository carReturnRepository;
 
+    CarReturn newCarReturn = buildCarReturn();
+
+    private CarReturn buildCarReturn() {
+        CarReturn carReturn = new CarReturn();
+        carReturn.setDateOfReturn(LocalDate.of(2019, 11, 11));
+        carReturn.setAdditionalPayment(22.0);
+        carReturn.setComments("first car");
+        return carReturn;
+    }
+
+    CarReturn createdCarReturn;
+
+    @BeforeEach
+    public void beforeEach() {
+        createdCarReturn = this.carReturnRepository.saveAndFlush(newCarReturn);
+    }
+
+    @AfterEach
+    public void afterEach() {
+        this.carReturnRepository.deleteAll();
+    }
+
     @Test
     public void givenCarReturnDetails_whenAPostRequestReceived_thenCreateCarReturn() {
 
         CarReturnDto carReturnDetails = CarReturnDto.carReturnDto()
                 .withDateOfReturn(LocalDate.of(2019, 11, 11))
                 .withAdditionalPayment(22.0)
-                .withComments("first car");
+                .withComments("second car");
 
         String relativePath = "/car-return";
-
         ResponseEntity<CarReturnDto> actualResponse = this.restTemplate.postForEntity(url(relativePath), carReturnDetails, CarReturnDto.class);
 
         Long newId = actualResponse.getBody().id;
+        Optional<CarReturn> expectedCarReturn = this.carReturnRepository.findById(newId);
         CarReturnDto expectedResponse = carReturnDetails.withId(newId);
-        Optional<CarReturn> expectedCarReturn = carReturnRepository.findById(newId);
 
-        Assertions.assertNotNull(expectedCarReturn.isPresent());
+        Assertions.assertTrue(expectedCarReturn.isPresent());
         Assertions.assertEquals(expectedResponse, actualResponse.getBody());
-
     }
 
     @Test
     public void givenExistingId_whenGetCarReturnById_thenReturnCarReturn() {
 
-        CarReturn carReturn = new CarReturn();
-        carReturn.setDateOfReturn(LocalDate.of(2019, 11, 11));
-        carReturn.setAdditionalPayment(22.0);
-        carReturn.setComments("first car");
-
-        carReturn = carReturnRepository.saveAndFlush(carReturn);
+        CarReturn savedCarReturn = this.createdCarReturn;
 
         CarReturnDto expectedResponse = CarReturnDto.carReturnDto()
-                .withId(carReturn.getId())
-                .withDateOfReturn(carReturn.getDateOfReturn())
-                .withAdditionalPayment(carReturn.getAdditionalPayment())
-                .withComments(carReturn.getComments());
+                .withId(savedCarReturn.getId())
+                .withDateOfReturn(savedCarReturn.getDateOfReturn())
+                .withAdditionalPayment(savedCarReturn.getAdditionalPayment())
+                .withComments(savedCarReturn.getComments());
 
-        String relativePath = "/car-return/" + carReturn.getId();
-
+        String relativePath = "/car-return/" + savedCarReturn.getId();
         ResponseEntity<CarReturnDto> actualResponse = this.restTemplate.getForEntity(url(relativePath), CarReturnDto.class);
 
         Assertions.assertEquals(expectedResponse, actualResponse.getBody());
         Assertions.assertEquals(HttpStatus.OK, actualResponse.getStatusCode());
-
     }
 
     @Test
     public void givenNewCarReturnDetails_WhenPutRequestIsReceived_ThenUpdateCarReturn() {
 
-        CarReturn carReturn = new CarReturn();
-        carReturn.setDateOfReturn(LocalDate.of(2019, 11, 11));
-        carReturn.setAdditionalPayment(22.0);
-        carReturn.setComments("first car");
+        CarReturn savedCarReturn = this.createdCarReturn;
 
-        carReturn = carReturnRepository.saveAndFlush(carReturn);
-
-        CarReturnDto newCarReturnDto = CarReturnDto.carReturnDto()
+        CarReturnDto newCarReturnDetails = CarReturnDto.carReturnDto()
                 .withDateOfReturn(LocalDate.of(2020, 11, 11))
                 .withAdditionalPayment(33.0)
                 .withComments("first car updated");
 
-        String relativePath = "/car-return/" + carReturn.getId();
+        String relativePath = "/car-return/" + savedCarReturn.getId();
+        this.restTemplate.put(url(relativePath), newCarReturnDetails);
 
-        this.restTemplate.put(url(relativePath), newCarReturnDto);
-
-        CarReturn updatedEntity = carReturnRepository.findById(carReturn.getId()).get();
+        CarReturn updatedCarReturn = this.carReturnRepository.findById(savedCarReturn.getId()).get();
 
         CarReturnDto verifyUpdateDto = CarReturnDto.carReturnDto()
-                .withDateOfReturn(updatedEntity.getDateOfReturn())
-                .withAdditionalPayment(updatedEntity.getAdditionalPayment())
-                .withComments(updatedEntity.getComments());
+                .withDateOfReturn(updatedCarReturn.getDateOfReturn())
+                .withAdditionalPayment(updatedCarReturn.getAdditionalPayment())
+                .withComments(updatedCarReturn.getComments());
 
-        Assertions.assertEquals(newCarReturnDto, verifyUpdateDto);
-
+        Assertions.assertEquals(newCarReturnDetails, verifyUpdateDto);
     }
 
     @Test
     public void givenExistingCarReturn_WhenDeleteRequestIsReceived_ThenCarRetuenIsDeleted() {
 
-        CarReturn carReturn = new CarReturn();
-        carReturn.setDateOfReturn(LocalDate.of(2019, 11, 11));
-        carReturn.setAdditionalPayment(22.0);
-        carReturn.setComments("first car");
+        CarReturn savedCarReturn = this.createdCarReturn;
 
-        carReturn = carReturnRepository.saveAndFlush(carReturn);
+        String relativePath = "/car-return/" + savedCarReturn.getId();
+        this.restTemplate.delete(relativePath, savedCarReturn.getId());
 
-        String relativePath = "/car-return/" + carReturn.getId();
-        this.restTemplate.delete(relativePath, carReturn.getId());
-
-        Optional<CarReturn> deletedCarReturn = this.carReturnRepository.findById(carReturn.getId());
+        Optional<CarReturn> deletedCarReturn = this.carReturnRepository.findById(savedCarReturn.getId());
 
         Assertions.assertFalse(deletedCarReturn.isPresent());
-
     }
 }
