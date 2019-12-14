4c8a13e9dac7a266e7d6306ee14ab24dc27695f2
Arthovis <andreifbaicu@gmail.com>
2019-12-14 10:40:43 +0200

    Finished the BranchRepository interface class

--- src/main/java/com/sda10/carrental/model/Branch.java
+++ src/main/java/com/sda10/carrental/model/Branch.java
@@ -1,4 +1,14 @@
 package com.sda10.carrental.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "branch")
 public class Branch {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+
 }
