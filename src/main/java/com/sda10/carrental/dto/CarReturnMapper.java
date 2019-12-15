6beb432227bca01223bfd62a1015bba761b016c3
DanNistor1 <dan.nistor1@yahoo.ro>
2019-12-15 12:26:59 +0200

    Removed fakeMapper from CarReturnMapper

--- src/main/java/com/sda10/carrental/dto/CarReturnMapper.java
+++ src/main/java/com/sda10/carrental/dto/CarReturnMapper.java
@@ -1,42 +1,31 @@
 package com.sda10.carrental.dto;
 
 import com.sda10.carrental.model.CarReturn;
-import com.sda10.carrental.model.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CarReturnMapper {
 
+    @Autowired
+    private EmployeeMapper employeeMapper;
+
     public CarReturn toEntity(CarReturnDto carReturnDtoDetails) {
         CarReturn carReturn = new CarReturn();
-        carReturn.setEmployee(fakeEmployeeMapper(carReturnDtoDetails.employeeDto));
+        carReturn.setEmployee(employeeMapper.toEntity(carReturnDtoDetails.employeeDto));
         carReturn.setDateOfReturn(carReturnDtoDetails.dateOfReturn);
         carReturn.setAdditionalPayment(carReturnDtoDetails.additionalPayment);
         carReturn.setComments(carReturnDtoDetails.comments);
         return carReturn;
     }
 
-    public Employee fakeEmployeeMapper(EmployeeDto employeeDto) {
-        Employee employee = new Employee();
-        employee.setId(employeeDto.id);
-        employee.setJobPosition(employeeDto.jobPosition);
-        employee.setNameAndSurname(employeeDto.nameAndSurname);
-        return employee;
-    }
-
     public CarReturnDto toDto(CarReturn carReturn) {
         return CarReturnDto.carReturnDto()
                 .withId(carReturn.getId())
-                .withEmployeeDto(fakeEmployeeDtoMapper(carReturn.getEmployee()))
+                .withEmployeeDto(employeeMapper.toDto(carReturn.getEmployee()))
                 .withDateOfReturn(carReturn.getDateOfReturn())
                 .withAdditionalPayment(carReturn.getAdditionalPayment())
                 .withComments(carReturn.getComments());
     }
 
-    public EmployeeDto fakeEmployeeDtoMapper(Employee employee) {
-        return EmployeeDto.employeeDto()
-                .withId(employee.getId())
-                .withNameAndSurname(employee.getNameAndSurname())
-                .withJobPosition(employee.getJobPosition());
-    }
 }
