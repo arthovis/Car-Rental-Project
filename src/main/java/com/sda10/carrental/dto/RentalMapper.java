8ffaa4a8fe137d6a955b1b1d1716cd1ab2f79ab2
DanNistor1 <dan.nistor1@yahoo.ro>
2019-12-16 11:32:30 +0200

    add employee and branch to Rental, RentalDto, RentalMapper, RentalControllerRestIntegrationTest

--- src/main/java/com/sda10/carrental/dto/RentalMapper.java
+++ src/main/java/com/sda10/carrental/dto/RentalMapper.java
@@ -1,16 +1,27 @@
 package com.sda10.carrental.dto;
 
 import com.sda10.carrental.model.Rental;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 
 @Component
 public class RentalMapper {
+
+    @Autowired
+    private EmployeeMapper employeeMapper;
+
+    @Autowired
+    private BranchMapper branchMapper;
+
+
     public Rental toEntity(RentalDto dto) {
         Rental rental = new Rental();
         rental.setId(dto.id);
         rental.setRentalDate(dto.rentalDate);
         rental.setComments(dto.comments);
+        rental.setEmployee(employeeMapper.toEntity(dto.employeeDto));
+        rental.setBranch(branchMapper.toEntity(dto.branchDto));
         return rental;
     }
 
@@ -18,6 +29,8 @@ public class RentalMapper {
         return RentalDto.rentalDto()
                 .withId(entity.getId())
                 .withRentalDate(entity.getRentalDate())
-                .withComments(entity.getComments());
+                .withComments(entity.getComments())
+                .withEmployeeDto(employeeMapper.toDto(entity.getEmployee()))
+                .withBranchDto(branchMapper.toLightDto(entity.getBranch()));
     }
 }
