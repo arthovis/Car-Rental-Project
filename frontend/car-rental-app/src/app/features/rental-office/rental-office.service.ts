970de774c5df334be0a1cfc7fd2789fbb8ac11eb
Arthovis <andreifbaicu@gmail.com>
2020-01-11 12:19:44 +0200

    Added Rental Office Service in front-end v2.0

---
+++ frontend/car-rental-app/src/app/features/rental-office/rental-office.service.ts
@@ -0,0 +1,21 @@
+import { RentalOffice } from './../../shared/model/rentalOffice';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RentalOfficeService {
+
+  private readonly RENTAL_OFFICE_API = `${environment.serverApiUrl}/rental-office`;
+
+  constructor(private httpClient: HttpClient) { }
+
+  saveRentalOffice(rentalOffice: RentalOffice): Observable<RentalOffice> {
+    return this.httpClient.post<RentalOffice>(this.RENTAL_OFFICE_API, rentalOffice);
+  }
+}
+
